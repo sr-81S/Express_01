4d92565fbcde5906dd0e4f261d53149447450904
saroz savar <sarojsabar12345@gmail.com>
2022-05-03 18:30:38 +0530

    submit

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const format = require('date-format')
+
+//swaggers related file
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 const PORT = process.env.PORT || 4000;
 
 
